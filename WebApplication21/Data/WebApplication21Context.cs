71b69d790bac2bc6bb30a063586f72994ff46784
sowjanya.gollapathi <sowjanya.gollapathi@gmail.com>
2022-01-07 21:55:03 +0530

    Add project files.

---
+++ WebApplication21/Data/WebApplication21Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication21.Model;
+
+namespace WebApplication21.Data
+{
+    public class WebApplication21Context : DbContext
+    {
+        public WebApplication21Context (DbContextOptions<WebApplication21Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication21.Model.Employee> Employee { get; set; }
+    }
+}
---
+++ WebApplication21/Model/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication21.Model
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNo { get; set; }
+    
+    }
+}
